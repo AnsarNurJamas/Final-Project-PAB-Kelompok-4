15cfc24383398626216ec82a4b48da30f6176b81
Ansar Nur Jamas <jamasguitarist@gmail.com>
2023-11-04 19:24:50 +0700

    header & bottom tabs

---
+++ screens/about.js
@@ -0,0 +1,12 @@
+import { Header } from "../components";
+import { Box, ScrollView } from "native-base";
+
+const Home = () => {
+    return (
+        <>
+          <Header title={"ABOUT"} />
+        </>
+    );
+};
+
+export default Home;
---
+++ screens/home.js
@@ -0,0 +1,12 @@
+import { Header } from "../components";
+import { Box, ScrollView, Image } from "native-base";
+
+const Home = () => {
+    return (
+        <>
+          <Header title={"FISHEESH"} />
+        </>
+    );
+};
+
+export default Home;
