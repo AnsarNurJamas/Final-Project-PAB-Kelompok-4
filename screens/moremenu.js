aab4b5ffd5d44e1df7fdd78279fe435fac4b20de
unknown <dennysby55@gmail.com>
2023-11-08 20:42:14 +0700

    membuat detail history dan history produk

--- screens/moremenu.js
+++ screens/moremenu.js
@@ -7,6 +7,7 @@ import Profile from "./profile";
 import Article from "../screens/article";
 import Faq from "./faq";
 
+
 const Stack = createNativeStackNavigator();
 
 const Moremenu = ({ navigation }) => {
